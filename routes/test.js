382ad0528e60b934d9db2d155bf7ae6e37321388
Partha Biswas <partha4224@gmail.com>
2021-06-27 21:49:14 +0530

    fix

--- routes/test.js
+++ routes/test.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {
-  performance,
-  PerformanceObserver
-} = require('perf_hooks');
 
 
 
 router.get('/status', (req,res)=> {
-    console.log(c);
     return res.send({'status':200,'message':'Healthy'});
     
 });
